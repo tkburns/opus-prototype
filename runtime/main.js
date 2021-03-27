d1e7323e13878e8385e74af2146d8be085dbe985
Tate Burns <tate.burns@lifeway.com>
2021-03-27 10:41:52 -0500

    feat: add basic runtime

---
+++ runtime/main.js
@@ -0,0 +1,22 @@
+/*
+  ---------------------------------
+    opus runtime
+  ---------------------------------
+*/
+
+/* eslint-disable no-unused-vars */
+/*
+  disabling no-unused-vars since this file will be injected into
+  the output, so the runtime functions are used without being exported
+*/
+
+// this is just for debugging now
+// eventually logging will be handled through effects
+// eslint-disable-next-line no-console
+const print = (text) => console.log(text);
+
+/*
+  ---------------------------------
+    end of opus runtime
+  ---------------------------------
+*/
