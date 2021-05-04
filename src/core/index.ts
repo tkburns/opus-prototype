bbaf30d4265b4634519219d294da8aa1dbcb60f9
Tate Burns <tate@burnskids.com>
2021-05-03 21:33:40 -0500

    feat: add output flag

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,13 +1,39 @@
-import { system } from '&/utils/system/system';
+import { collect } from '&/utils/system/collect';
+import { system, System } from '&/utils/system/system';
 import { codeGenerator } from './code-generation';
-import { lexer, tokenFilter } from './lexer';
+import { FilteredToken, lexer, Token, tokenFilter } from './lexer';
 import { parser } from './parser';
 import { injectRuntime } from './runtime';
+import { astStringifier, tokenStringifier } from './stringification';
 
-export const core = system(
-  lexer,
-  tokenFilter,
-  parser,
-  codeGenerator,
-  injectRuntime
-);
+export const core = (output?: string): System<string, string> => {
+  if (output === 'all-tokens') {
+    return system(
+      lexer,
+      collect<Token>(),
+      tokenStringifier
+    );
+  } else if (output === 'tokens') {
+    return system(
+      lexer,
+      tokenFilter,
+      collect<FilteredToken>(),
+      tokenStringifier
+    );
+  } else if (output === 'ast') {
+    return system(
+      lexer,
+      tokenFilter,
+      parser,
+      astStringifier
+    );
+  } else {
+    return system(
+      lexer,
+      tokenFilter,
+      parser,
+      codeGenerator,
+      injectRuntime
+    );
+  }
+};
