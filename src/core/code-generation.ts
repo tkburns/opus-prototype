6bbddf3c7cf41fb73feffb4b68d4dd6f1524be67
Tate Burns <tate@burnskids.com>
2021-11-08 22:44:34 -0600

    refactor(code-gen): use manual function calls instead of minimal tree-walker

--- src/core/code-generation.ts
+++ src/core/code-generation.ts
@@ -1,46 +1,83 @@
-import { code, lines } from '&/utils/system/stringification';
-import { Walkers, createWalkerModule } from '&/utils/system/tree-walker';
+import { Module } from '&/utils/system/system';
+import { lines, code } from '&/utils/system/stringification';
 import type * as AST from './ast.types';
+import { transformByType } from '&/utils/system/tree-walker';
 
 
-const walkers: Walkers<AST.Node, string> = {
-  'program': (node, process) => lines(
-    ...node.entries
-      .map(process)
-      .flatMap(code => [code, '']),
-  ),
+const program = (node: AST.Program) => lines(
+  ...node.entries
+    .map(node => node.type === 'declaration' ? declaration(node) : expression(node))
+    .flatMap(code => [`${code};`, '']),
+);
 
-  'declaration': (node, process) =>
-    `const ${process(node.name)} = ${process(node.expression)}`,
 
-  'function-application': (node, process) => {
-    const fn = process(node.func);
-    const arg = process(node.arg);
+const declaration = (node: AST.Declaration) =>
+  `const ${name(node.name)} = ${expression(node.expression)}`;
 
-    return ['name', 'function-application'].includes(node.func.type)
-      ? `${fn}(${arg})`
-      : `(${fn})(${arg})`;
-  },
+const expression = (node: AST.Expression): string => transformByType(node, {
+  'function-application': funcApplication,
+  match,
+  'function': func,
+  tuple,
+  name,
+  atom,
+  number,
+  text
+});
 
-  'match': (node, process) => code`
-    runMatch(${process(node.principal)}, [
-      ${node.clauses.map(process).join(',\n')}
-    ])
-  `,
-  'match-clause': (node, process) =>
-    `[${process(node.pattern)}, () => ${process(node.body)}]`,
-  'value-pattern': (node, process) => `{ type: 'value', value: ${process(node.value)} }`,
-  'wildcard-pattern': () => '{ type: \'wildcard\' }',
 
-  'function': (node, process) =>
-    `(${process(node.arg)}) => ${process(node.body)}`,
-  'tuple': (node, process) =>
-    `[${node.members.map(process).join(', ')}]`,
+const funcApplication = (node: AST.FuncApplication) =>{
+  const fn = expression(node.func);
+  const arg = expression(node.arg);
 
-  'name': (node) => node.value,
-  'atom': (node) => `Symbol.for('${node.value}')`,
-  'number': (node) => node.token.source,
-  'text': (node) => `"${node.value}"`,
+  return ['name', 'function-application'].includes(node.func.type)
+    ? `${fn}(${arg})`
+    : `(${fn})(${arg})`;
 };
 
-export const codeGenerator = createWalkerModule(walkers);
+
+const match = (node: AST.Match) => code`
+  runMatch(${expression(node.principal)}, [
+    ${node.clauses.map(matchClause).map(item => `${item},`)}
+  ])
+`;
+
+const matchClause = (node: AST.MatchClause) =>
+  `[${pattern(node.pattern)}, () => ${expression(node.body)}]`;
+
+const pattern = (node: AST.Pattern) => transformByType(node, {
+  'value-pattern': valuePattern,
+  'wildcard-pattern': wildcardPattern
+});
+
+const valuePattern = (node: AST.ValuePattern) =>
+  `{ type: 'value', value: ${expression(node.value)} }`;
+
+const wildcardPattern = (node: AST.WildcardPattern) =>
+  '{ type: \'wildcard\' }';
+
+
+const func = (node: AST.Func) =>
+  `(${name(node.arg)}) => ${expression(node.body)}`;
+
+const tuple = (node: AST.Tuple) =>
+  `[${node.members.map(expression).join(', ')}]`;
+
+const name = (node: AST.Name) => node.value;
+
+const atom = (node: AST.Atom) => `Symbol.for('${node.value}')`;
+
+const number = (node: AST.Numeral) => node.token.source;
+
+const text = (node: AST.Text) => `"${node.value}"`;
+
+
+export const codeGenerator: Module<AST.Node, string> = {
+  run: (node) => {
+    if (node.type !== 'program') {
+      throw new Error(`cannot generate code directly for a ${node.type}; only 'program' is supported`);
+    }
+
+    return program(node);
+  }
+};
